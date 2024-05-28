130602a66cb7bd356dd27a960847df981833821f
AbdusamadAbdurahimov <survolgeyms@gmail.com>
2024-05-28 12:19:08 +0500

    JS-2-dars
    ZeroMax-IT Center-35-guruh

---
+++ Js/main.js
@@ -0,0 +1,89 @@
+
+// if("Salom" == "Salom"){
+//     console.log("Ular teng!");
+// }
+
+// else{
+//     console.log("Ular teng emas !")
+// }
+
+// if(45 >= 45){
+//     console.log ("Ular teng yoki katta!");
+// }
+// else{
+//     console.log("Ular teng emas yoki katta emas!")
+// }
+
+// if(69*45/10*80+120-90+30 >= 383*100/69*10*10/20){
+//     console.log ("Ular teng yoki katta!");
+// }
+// else{
+//     console.log("Ular teng emas yoki katta emas!")
+// }
+
+// if("Doni Chikibamboni" == "Doni Chikibamboni"){
+//     console.log("Ular teng!");
+// }
+
+// else{
+//     console.log("Ular teng emas !")
+// }
+
+// if(45 <=){
+//     console.log("Solishtitilayotgan son 45ga yoki katta !");
+// }
+// else{
+//     console.log("Solishtitilayotgan son 45ga teng emas yoki katta emas!")
+// }
+
+// if(45 == 45){
+//     console.log("1-shart bajarildi !");
+// }
+// else if(45 == 45){
+//     console.log("2-shart bajarildi!")
+// }
+// else{
+//     console.log("Yuqoridagi shartlardan birontasi bajarilmadi!")
+// }
+
+// if(100 == 99){
+//     console.log("Doni 1 darsga kech qoldi");
+// }
+// else if(200 == 200){
+//     console.log("Doni kech colmadi")
+// }
+// else{
+//     console.log("Doni darsga kelmadi")
+// }
+
+// let a =45;
+// let b =45;
+
+// if(a>=b){
+// console.log ("ular teng !");
+// }
+// else{
+//     console.log("a ning qiymati topilmadi!")
+// }
+
+// let a = 46;
+// let b =10;
+// let c =19;
+// let d =49;
+
+// if(a == 47 && b == 11 && c == 18 && d == 48){
+//     console.log("Ishladi");
+// }
+// else{
+//     console.log("Ishlamadi")
+// }
+
+// if(a == 47 || b == 10 || c == 19 || d == 49){
+//     console.log("Shartlarning kamida bittasi yoki hammasi to'g'ri!");
+// }
+// else{
+//     console.log("Shartlarning hammasi nito'g'ri!");
+// }
+
+
+
